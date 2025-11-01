a45c69e6ea89162c63f5cacc6498e282686de414
MohsinEzaidi <mohsinezzaidi@gmail.com>
2025-11-01 12:16:47 +0100

    fix strlcat and protect memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mezzaidi <mezzaidi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 17:22:29 by mezzaidi          #+#    #+#             */
-/*   Updated: 2025/10/29 20:33:11 by mezzaidi         ###   ########.fr       */
+/*   Updated: 2025/11/01 12:15:43 by mezzaidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t			i;
 	unsigned char	*temdst;
 	unsigned char	*temsrc;
-
+	
+	if (!dst && !src)
+		return (NULL);
+	if (dst == src)
+		return (dst);
 	temdst = (unsigned char *) dst;
 	temsrc = (unsigned char *) src;
-	if (!temdst && !temsrc)
-		return (NULL);
 	i = 0;
 	while (i < n)
 	{
